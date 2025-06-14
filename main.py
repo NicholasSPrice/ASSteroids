4fd014717c1de0f454d5c4759d60e1c2a6c17e3f
NSPrice <nick@nsprice.io>
2025-06-13 22:59:24 -0400

    show player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame, player, circleshape
 from constants import *
 
 def main():
@@ -11,19 +11,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Creation of the player
+    x_p = SCREEN_WIDTH / 2
+    y_p = SCREEN_HEIGHT / 2
+    mav = player.Player(x_p, y_p)
+    
+    
     # gameloop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        mav.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
         main()
