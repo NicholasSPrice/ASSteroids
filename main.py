4a36da0e9f93ad07cb47590338420bd94daa17c9
NSPrice <nick@nsprice.io>
2025-06-13 21:57:51 -0400

    Limited FPS to 60.

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def main():
     running = True
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # gameloop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
     print("Starting Asteroids!")
