cbb3ef93c666af6358d0d5a175d1022f6e990242
NSPrice <nick@nsprice.io>
2025-06-15 14:52:29 -0400

    using groups to manage the objects now

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame, player, circleshape
+import pygame 
+from player import Player
 from constants import *
 
 def main():
-    running = True
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    mav = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
-    # Creation of the player
-    x_p = SCREEN_WIDTH / 2
-    y_p = SCREEN_HEIGHT / 2
-    mav = player.Player(x_p, y_p)
-    
-    
     # gameloop
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+            
         screen.fill("black")
-        mav.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
         main()
